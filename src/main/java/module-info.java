203376b0ad4565bd6991afcc3254a67c86152585
GreenzQe <73607229+GreenzQe@users.noreply.github.com>
2024-11-27 11:41:41 +0100

    added java.naming

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module dk.easv.mytunes {
     requires javafx.fxml;
     requires java.sql;
     requires com.microsoft.sqlserver.jdbc;
+    requires java.naming;
 
 
     opens dk.easv.mytunes to javafx.fxml;
