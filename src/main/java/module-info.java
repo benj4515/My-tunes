4cced765a80c660fdb4ce6f5fff7135686f5a801
GreenzQe <73607229+GreenzQe@users.noreply.github.com>
2024-11-27 09:58:27 +0100

    Added DBConnector

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module dk.easv.mytunes {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires com.microsoft.sqlserver.jdbc;
 
 
     opens dk.easv.mytunes to javafx.fxml;
